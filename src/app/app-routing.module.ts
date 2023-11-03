60b3fd42020376da9332b33e56f4b64c919e6e8b
MhdRabeai <mhd.rabea.naser@gmail.com>
2023-11-03 16:46:01 +0300

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,25 @@ import { HomeComponent } from './components/home/home.component';
 import { SkillsComponent } from './components/skills/skills.component';
 import { WorkComponent } from './components/work/work.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { ServicesComponent } from './components/services/services.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'home', component: HomeComponent },
-  { path: 'skills', component: SkillsComponent },
-  { path: 'work', component: WorkComponent },
-  { path: 'contact', component: ContactComponent },
-  { path: '**', redirectTo: '/' },
+  { path: '', pathMatch: 'full', redirectTo: '/home' },
+  { path: 'home', component: HomeComponent, data: { animation: 'home' } },
+
+  { path: 'skills', component: SkillsComponent, data: { animation: 'skills' } },
+  {
+    path: 'services',
+    component: ServicesComponent,
+    data: { animation: 'services' },
+  },
+  { path: 'work', component: WorkComponent, data: { animation: 'work' } },
+  {
+    path: 'contact',
+    component: ContactComponent,
+    data: { animation: 'contact' },
+  },
+  { path: '**', redirectTo: '/', data: { animation: 'home' } },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,8 @@ import { ContactComponent } from './components/contact/contact.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { SafeHtmlPipe } from './shared/safe-html.pipe';
-
+import { ServicesComponent } from './components/services/services.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent,
@@ -19,8 +20,15 @@ import { SafeHtmlPipe } from './shared/safe-html.pipe';
     WorkComponent,
     ContactComponent,
     SafeHtmlPipe,
+    ServicesComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
